e386bbfee99949e4fecc1c6433322d690b68c4c0
ruslanliska <ruslan.liska@gmail.com>
2024-11-04 18:44:47 +0200

    modify imports

--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
+import os
+import sys
+
 from src.ui.main_window import LinkedInAutomationApp
 
+# Add the parent directory to PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 if __name__ == '__main__':
+
     app = LinkedInAutomationApp()
     app.mainloop()
