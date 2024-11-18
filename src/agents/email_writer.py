b9c06f2884eadeac8f54e0ed6400d67fff7f504c
ruslanliska <ruslan.liska@gmail.com>
2024-11-18 10:36:04 +0200

    Add instruction and email reference to prompts

--- src/agents/email_writer.py
+++ src/agents/email_writer.py
@@ -6,13 +6,8 @@ from src.config import settings
 
 llm = ChatOpenAI(model='gpt-4o-mini', api_key=settings.OPENAI_API_KEY)
 
-
-SYSTEM_PROMPT = """Write an email to a prospective employer to introduce fitting candidates for their company based on the experience summary provided. A receiver profile will be given, so the email must be addressed directly to this person.
-
-Use the information from the candidate's experience summary to craft a compelling narrative about how your proposed candidates have similar qualities and are an excellent match for the company.
-
-**Note**: Do not mention or imply direct experience of the receiver or any specific individual. Instead, position the proposed candidates as representative of ideal experience aligned with the company's needs and existing goals. Avoid focusing on the receiver's experience.
-
+DEFAULT_EMAIL_INSTRUCTION = """
+Here is email instructions:
 - Begin with a friendly greeting.
 - Highlight your understanding of the company's needs based on the experience shared.
 - Address the receiver directly using the information in the profile.
@@ -29,19 +24,50 @@ Use the information from the candidate's experience summary to craft a compellin
 6. **Closing**: End the email with a respectful and polite sign-off.
 """
 
-PROMPT_TEMPLATE = ChatPromptTemplate.from_messages(
-    [
-        ('system', SYSTEM_PROMPT),
-        ('human', 'Here is receiver profile: {profile_summary}'),
-    ],
-)
+
+SYSTEM_PROMPT = """Write an email to a prospective employer to introduce fitting candidates for their company based on the experience summary provided. A receiver profile will be given, so the email must be addressed directly to this person.
+
+Use the information from the candidate's experience summary to craft a compelling narrative about how your proposed candidates have similar qualities and are an excellent match for the company.
+
+**Note**: Do not mention or imply direct experience of the receiver or any specific individual. Instead, position the proposed candidates as representative of ideal experience aligned with the company's needs and existing goals. Avoid focusing on the receiver's experience.
+You must write only email body, no subject
+User will provide some email reference/instructions:
 
 
-def generate_email(page_summary: str) -> str:
-    email_chain = PROMPT_TEMPLATE | llm | StrOutputParser()
+"""
+
+DEFAULT_USER_PROMPT = 'Generate email in english.'
+
+
+def generate_prompt_template(
+    user_prompt: str,
+    email_instructions: str,
+) -> ChatPromptTemplate:
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ('system', SYSTEM_PROMPT),
+            ('human', email_instructions),
+            ('human', user_prompt),
+            ('human', 'Here is receiver profile: {profile_summary}'),
+        ],
+    )
+    return prompt
+
+
+def generate_email(
+    page_summary: str,
+    user_prompt: str = DEFAULT_USER_PROMPT,
+    email_instructions: str = DEFAULT_EMAIL_INSTRUCTION,
+) -> str:
+    prompt = generate_prompt_template(
+        user_prompt=user_prompt,
+        email_instructions=email_instructions,
+    )
+    email_chain = prompt | llm | StrOutputParser()
     email = email_chain.invoke(
         {
             'profile_summary': page_summary,
+            'email_instructions': email_instructions,
         },
     )
     return email
