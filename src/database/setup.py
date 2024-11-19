b3b15ecfb282380d882f9a2e9a3fab0db5cd09c4
ruslanliska <ruslan.liska@gmail.com>
2024-11-19 10:17:51 +0200

    Create db for emails

--- src/database/setup.py
+++ src/database/setup.py
@@ -1,29 +1,39 @@
-# src/database/db_setup.py
-import os
 import sqlite3
 
-# Define the path to your database file
 DB_PATH = 'run_history.db'
 
 
 def create_database():
-    # Check if the database file already exists
-    if not os.path.exists(DB_PATH):
-        connection = sqlite3.connect(DB_PATH)
-        cursor = connection.cursor()
-        # SQL query to create the run_history table
-        create_table_query = """
-        CREATE TABLE IF NOT EXISTS run_history (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-            file_name TEXT,
-            status TEXT,
-            error_message TEXT
-        )
-        """
-        cursor.execute(create_table_query)
-        connection.commit()
-        connection.close()
-        print("SQLite database and 'run_history' table created successfully!")
-    else:
-        print('Database already exists. No action needed.')
+    connection = sqlite3.connect(DB_PATH)
+    cursor = connection.cursor()
+
+    # Create runs table
+    create_runs_table_query = """
+    CREATE TABLE IF NOT EXISTS runs (
+        run_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+        file_name TEXT,
+        status TEXT,
+        error_message TEXT
+    )
+    """
+    cursor.execute(create_runs_table_query)
+
+    # Create emails table
+    create_emails_table_query = """
+    CREATE TABLE IF NOT EXISTS emails (
+        email_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        run_id INTEGER,
+        linkedin_profile_url TEXT,
+        email_text TEXT,
+        email_status TEXT,
+        error_message TEXT,
+        timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (run_id) REFERENCES runs(run_id)
+    )
+    """
+    cursor.execute(create_emails_table_query)
+
+    connection.commit()
+    connection.close()
+    print('SQLite database setup complete')
