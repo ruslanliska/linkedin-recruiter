c08cf5316ccd715316a077ccf0f218c8ee2532b1
ruslanliska <ruslan.liska@gmail.com>
2024-11-19 08:40:36 +0200

    Add database setup

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 import os
 import sys
+
+from src.database.setup import create_database
+from src.ui.main_window import LinkedInAutomationApp
 # Add the parent directory to PYTHONPATH
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
-from src.ui.main_window import LinkedInAutomationApp
-
-
-
 if __name__ == '__main__':
+    create_database()
 
     app = LinkedInAutomationApp()
     app.mainloop()
