60f2805d24109864a70d7ec28fb7b06e8df27da6
ruslanliska <ruslan.liska@gmail.com>
2024-11-21 16:22:24 +0200

    Mpdify prompts

--- src/agents/email_writer.py
+++ src/agents/email_writer.py
@@ -7,6 +7,13 @@ from src.config import settings
 llm = ChatOpenAI(model='gpt-4o-mini', api_key=settings.OPENAI_API_KEY)
 
 DEFAULT_EMAIL_INSTRUCTION = """
+Write an email to a prospective employer to introduce fitting candidates for their company based on the experience summary provided. A receiver profile will be given, so the email must be addressed directly to this person.
+
+Use the information from the candidate's experience summary to craft a compelling narrative about how your proposed candidates have similar qualities and are an excellent match for the company.
+
+**Note**: Do not mention or imply direct experience of the receiver or any specific individual. Instead, position the proposed candidates as representative of ideal experience aligned with the company's needs and existing goals. Avoid focusing on the receiver's experience.
+You must write only email body, no subject, no signature.
+
 Here is email instructions:
 - Begin with a friendly greeting.
 - Highlight your understanding of the company's needs based on the experience shared.
@@ -25,14 +32,17 @@ Here is email instructions:
 """
 
 
-SYSTEM_PROMPT = """Write an email to a prospective employer to introduce fitting candidates for their company based on the experience summary provided. A receiver profile will be given, so the email must be addressed directly to this person.
+SYSTEM_PROMPT = """
+Based on reference email you get, you must generate similar email but personalized for receiver.
+Write an email and don't change not receiver relevant information from reference email.
+A receiver profile will be given, so the email must be addressed directly to this person.
 
-Use the information from the candidate's experience summary to craft a compelling narrative about how your proposed candidates have similar qualities and are an excellent match for the company.
+Use the information from the candidate's experience summary.
 
-**Note**: Do not mention or imply direct experience of the receiver or any specific individual. Instead, position the proposed candidates as representative of ideal experience aligned with the company's needs and existing goals. Avoid focusing on the receiver's experience.
+**Note**:
 You must write only email body, no subject, no signature.
-User will provide some email reference/instructions:
-
+If user provides some technologies or stack in reference, you must delete it and use stack and techologies from receiver profile_summary
+User will provide some email reference/instructions. Please, personalize email and write directly
 
 """
 
