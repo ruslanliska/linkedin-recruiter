b3345455d553ebbfb58a05ddfb451ad67150bc51
ruslanliska <ruslan.liska@gmail.com>
2025-04-03 15:22:56 +0300

    Maximize window

--- src/inmail/personalized_email.py
+++ src/inmail/personalized_email.py
@@ -95,6 +95,7 @@ def process_chunk_of_rows(
         time.sleep(random.uniform(2, 5))
         logger.info('ChromeDriver initialized successfully for this batch.')
         driver.get('https://www.linkedin.com/talent/search/advanced')
+        driver.maximize_window()
 
         logger.info('Search opened')
         time.sleep(random.uniform(2, 5))
@@ -104,21 +105,29 @@ def process_chunk_of_rows(
             # Wait for the element to be present (using a partial match for aria-label)
             element = WebDriverWait(driver, 15).until(
                 EC.presence_of_element_located(
-                    (By.CSS_SELECTOR, "button.facet-edit-button[data-view-name='search-facet-add'][aria-label*='Job titles']")
-                )
+                    (
+                        By.CSS_SELECTOR,
+                        "button.facet-edit-button[data-view-name='search-facet-add'][aria-label*='Job titles']",
+                    ),
+                ),
             )
-            print("Element found. Scrolling into view...")
+            print('Element found. Scrolling into view...')
             # Scroll the element into view
-            driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", element)
+            driver.execute_script(
+                "arguments[0].scrollIntoView({block: 'center'});", element,
+            )
             time.sleep(1)  # Wait a moment after scrolling
 
             # Now wait until it becomes clickable
             clickable_element = WebDriverWait(driver, 15).until(
                 EC.element_to_be_clickable(
-                    (By.CSS_SELECTOR, "button.facet-edit-button[data-view-name='search-facet-add'][aria-label*='Job titles']")
-                )
+                    (
+                        By.CSS_SELECTOR,
+                        "button.facet-edit-button[data-view-name='search-facet-add'][aria-label*='Job titles']",
+                    ),
+                ),
             )
-            print("Element is clickable. Clicking now...")
+            print('Element is clickable. Clicking now...')
             clickable_element.click()
             logger.info('Job title clicked')
             for title in job_titles:
