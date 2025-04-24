c1dbd9ef69e2c9854a237ae87555de170ce433c0
ruslanliska <ruslan.liska@gmail.com>
2025-04-24 13:17:42 +0300

    Add time 300

--- brouse/main.py
+++ brouse/main.py
@@ -29,7 +29,7 @@ initial_action_agent_1 = [
             'url': 'http://linkedin.com/talent/hire/1645278338/discover/recruiterSearch?savedSearch=urn%3Ali%3Ats_cap_saved_search%3A1907552162&savedSearchAction=GET&searchContextId=f3acd4d4-469e-4e0a-bf40-76e774eda1fc&searchHistoryId=20619306290&searchRequestId=cce9405e-aab0-4d53-b6cb-97cb19e3b094&start=0&uiOrigin=SAVED_SEARCH',
         },
     },
-    {'wait': {'seconds': 30}},
+    {'wait': {'seconds': 300}},
 ]
 initial_action_agent_2 = [
     {'wait': {'seconds': 30}},
